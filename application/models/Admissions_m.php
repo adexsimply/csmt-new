1db00a982fc74184326c4506d99e987ae66a764f
adetoyeshina@gmail.com <adexsimply>
2018-04-16 17:19:28 +0100

    Added Term and Level Lists

--- application/models/Admissions_m.php
+++ application/models/Admissions_m.php
@@ -38,6 +38,98 @@ class Admissions_m extends CI_Model {
 		$session_list = $get_session->row();
 		return $session_list;
 	}
+
+    public function get_term_list() {
+
+        $get_term = $this->db->select('t.*,u.username')->from('term_list t')->join('users as u', 't.added_by=u.id', 'left')->order_by('id', 'DESC')->get();
+        $term_list = $get_term->result();
+        return $term_list;
+    }
+
+
+    public function create_term_name()
+    {
+        $this->load->helper('url');
+
+        $user_id = $this->session->userdata('active_user')->id;        
+        if ($this->input->post('term_id'))
+        {           
+        $this->db->set('term_name', $this->input->post('term_name'));
+        $this->db->where('id', $this->input->post('term_id'));
+        $this->db->update('term_list');         
+        }
+        else {
+        $data = array(
+            'term_name' => $this->input->post('term_name'),
+            'added_by' => $user_id
+        );
+        $insert = $this->db->insert('term_list', $data);
+
+        return $insert;
+        }
+    }
+
+    function get_term_by_id(){
+        $id = $this->input->post('id');
+        $get_term = $this->db->select('*')->from('term_list')->where('id', $id)->get();
+        $term_list = $get_term->row();
+        return $term_list;
+    }
+
+    ///Levels
+	public function get_level_list() {
+
+		$get_level = $this->db->select('l.*,u.username')->from('level_list l')->join('users as u', 'l.added_by=u.id', 'left')->order_by('id', 'DESC')->get();
+		$level_list = $get_level->result();
+		return $level_list;
+	}
+
+
+    public function create_level_name()
+    {
+        $this->load->helper('url');
+        //Capture User id
+        $user_id = $this->session->userdata('active_user')->id; 
+        //If it's an update request(id)       
+        if ($this->input->post('level_id'))
+        {    
+        $data_level = array(
+            'level_name' => $this->input->post('level_name'),
+            'level_rank' => $this->input->post('level_rank')
+        );         
+        //$query_check_rank = $this->db->select('*')->from('level_list')->where('level_rank', $this->input->post('level_rank'))->get();
+        //$num_rows = $query_check_rank->num_rows();
+        //if ($num_rows=='0' OR  ){
+        $this->db->where('id', $this->input->post('level_id'));
+        $this->db->update('level_list', $data_level); 
+       // }
+   	       	
+        }//End of Update
+        //If it's a new request
+        else {
+        $data = array(
+            'level_name' => $this->input->post('level_name'),
+            'level_rank' => $this->input->post('level_rank'),
+            'added_by' => $user_id
+        );
+        $query_check_rank = $this->db->select('*')->from('level_list')->where('level_rank', $this->input->post('level_rank'))->get();
+        $num_rows = $query_check_rank->num_rows();
+        if ($num_rows=='0'){
+        $insert = $this->db->insert('level_list', $data);
+        }
+
+        return $insert;
+        }//End of New Request
+    }
+
+	function get_level_by_id(){
+		$id = $this->input->post('id');
+		$get_level = $this->db->select('*')->from('level_list')->where('id', $id)->get();
+		$level_list = $get_level->row();
+		return $level_list;
+	}
+
+		
 }
 
 ?>
